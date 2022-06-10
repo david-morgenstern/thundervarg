69b3edf3b8a45d3dc870a044955f923f9ccd02a2
daveyo89 <j.tothdavid@gmail.com>
2022-06-11 00:10:33 +0200

    separate authentication details

--- app/main.py
+++ app/main.py
@@ -1,21 +1,16 @@
 import os
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import sqlalchemy
 from fastapi import FastAPI, HTTPException, Depends, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_sqlalchemy import DBSessionMiddleware, db
-from passlib.context import CryptContext
-from jose import JWTError, jwt
-from pydantic import BaseModel
 
+from app.authenticate import Token, authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, oauth2_scheme, \
+    get_password_hash
 from app.create_initial_test_db import engine, create_tables
 from app.model import Todo, User
 from app.schema import SchemaTodoUpdate
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 app = FastAPI()
 
@@ -27,82 +22,6 @@ app.add_middleware(DBSessionMiddleware, db_url=sql_url)
 
 if not sqlalchemy.inspect(engine).has_table('user'):
     create_tables()
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(username: str) -> User:
-    with db.session as sess:
-        user = sess.query(User).filter(User.name == username).first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
-
-
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
-    if not verify_password(password, user.password):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(username=token_data.username)
-
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 
 @app.post("/token", response_model=Token)
@@ -119,7 +38,6 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         data={"sub": user.name}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
-
 
 
 @app.get("/private/")
@@ -145,7 +63,7 @@ def read_user(uid: int):
 def add_user(user: User):
     with db.session as sess:
         sess.expire_on_commit = False
-        new_user = User(name=user.name,  password=get_password_hash(user.password), disabled=user.disabled)
+        new_user = User(name=user.name, password=get_password_hash(user.password), disabled=user.disabled)
         sess.add(new_user)
         sess.commit()
         return new_user
